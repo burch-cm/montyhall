f27d4d8ccf1c28c63dff19e7394f37ffd9308b94
Kitt Burch <christopher.m.burch@gmail.com>
2019-12-19 10:02:01 -0500

    Update montyhall.R

--- montyhall.R
+++ montyhall.R
@@ -3,21 +3,21 @@ library(purrr)
 
 
 simple_hall <- function(strategy = "stay") {
-    stay <- sample(c("car", "goat", "goat"), 1)
+    stay <- sample(factor(c("car", "goat", "goat")), 1)
     if (stay == "car") {
-        switch <- "goat"
+        switch <- factor(c("goat"), levels = c("car", "goat"))
     } else {
-        switch <- "car"
+        switch <- factor(c("car"), levels = c("car", "goat"))
     }
     if (strategy == "stay") return(stay)
     return(switch)
 }
 
 run_halls <- function(n_halls, percentage = FALSE) {
-    stay_   <- purrr::map_chr(rep("stay", n_halls), simple_hall)
-    switch_ <- purrr::map_chr(rep("switch", n_halls), simple_hall)
-    stay_wins <- table(stay_)["car"]
-    switch_wins <- table(switch_)["car"]
+    stay_   <- purrr::map_int(rep("stay", n_halls), simple_hall)
+    switch_ <- purrr::map_int(rep("switch", n_halls), simple_hall)
+    stay_wins <- table(stay_)[1]
+    switch_wins <- table(switch_)[1]
     if (percentage) {
         stay_wins <- stay_wins / n_halls
         switch_wins <- switch_wins / n_halls
